2c5f4e2d1d7e4d99cb1d0d24bde0c037a56c15c0
Guilherme Freitas <105685817+GuiSatierf@users.noreply.github.com>
2023-01-17 18:47:42 -0300

    :P
    Falta
    -vídeo
    -outros modais
    -footer
    -responsive

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+//Modal//
+
+const modal = document.querySelector(".modal");
+const trigger = document.querySelector(".trigger");
+const closeButton = document.querySelector(".close-modal");
+
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
+
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
+
+//Carrosel//
