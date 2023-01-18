f0159a4a7f2fa156873928db74113113c934f2bd
Guilherme Freitas <105685817+GuiSatierf@users.noreply.github.com>
2023-01-18 18:49:47 -0300

    Backup

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,39 @@
-//Modal//
+//Modal One//
+const modalOne = document.querySelector(".modalOne");
+const triggerOne = document.querySelector(".triggerOne");
+const closeButtonOne = document.querySelector(".close-modalOne");
 
-const modal = document.querySelector(".modal");
-const trigger = document.querySelector(".trigger");
-const closeButton = document.querySelector(".close-modal");
 
 function toggleModal() {
-    modal.classList.toggle("show-modal");
+    modalOne.classList.toggle("show-modalOne");
 }
 
 function windowOnClick(event) {
-    if (event.target === modal) {
+    if (event.target === modalOne) {
         toggleModal();
     }
 }
 
-trigger.addEventListener("click", toggleModal);
-closeButton.addEventListener("click", toggleModal);
+
+triggerOne.addEventListener("click", toggleModal);
+closeButtonOne.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
 
-//Carrosel//
+//Modal two//
+const modalTwo = document.querySelector(".modalTwo");
+const triggerTwo = document.querySelector(".triggerTwo");
+const closeButtonTwo = document.querySelector(".close-modalTwo");
+
+function toggleModalTwo() {
+    modalTwo.classList.toggle("show-modalTwo");
+}
+
+function windowOnClickTwo(event) {
+    if (event.target === modalTwo) {
+       toggleModalTwo();
+    }
+}
+
+triggerTwo.addEventListener("click", toggleModalTwo);
+closeButtonTwo.addEventListener("click", toggleModalTwo);
+window.addEventListener("click", windowOnClickTwo);
